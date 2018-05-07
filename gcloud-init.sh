4a93db1c297780a7792c8ea7a9faccf1670f9b98
Glen D Sanford <9len@users.noreply.github.com>
2018-05-07 21:37:48 +0000

    [osstool] add prepare command (#5188)
    The `prepare` command does the following:
    - installs `LICENSE`
    - installs `CODE_OF_CONDUCT.md`
    - adds copyright comments to go, bash, yaml, and markdown files
    - optionally installs a go-appropriate `circle.yml` and a `README.md` if there isn't already one
    
    also rename the `init` command to `init-repo`.
    
    this will save me some trouble, since I'm about to do this for several different projects.
    
    fbshipit-source-id: 81e1689

--- gcloud-init.sh
+++ gcloud-init.sh
@@ -1,4 +1,5 @@
-#####################################################################
+#!/bin/bash
+
 # Copyright 2018 Turbine Labs, Inc.
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -10,9 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
 # implied. See the License for the specific language governing
 # permissions and limitations under the License.
-#####################################################################
-
-#!/bin/bash
 
 # Exit on any error
 set -e
